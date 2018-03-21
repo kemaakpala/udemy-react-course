c5597369f230fd42c46be71ec55ed739eb0d3c31
Patrick Akpala <kemaakpala@gmail.com>
2018-03-21 09:05:19 +0000

    optimization of react rendering and updates using shouldComponentUpdate() and PureComponents

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react'
+import React, {PureComponent} from 'react'
 import classes from './App.css'
 import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
 
-class App extends Component {
+class App extends PureComponent {
   constructor(props){
     super(props)
     console.log('[App.js] inside constructor:', props)
@@ -37,11 +37,12 @@ class App extends Component {
   componentDidMount(){
     console.log('[App.js] Inside componentDidMount')
   }
-  
-  shouldComponentUpdate(nextProps, nextState){
-      console.log('[Updated App.js] Inside shouldComponentUpdate()', nextProps, nextState)
-      return true
-  }
+
+  // shouldComponentUpdate(nextProps, nextState){
+  //     console.log('[Updated App.js] Inside shouldComponentUpdate()', nextProps, nextState)
+  //     return nextState.persons !== this.state.persons ||
+  //       nextState.showPersons !== this.state.showPersons
+  // }
 
   componentWillUpdate(nextProps, nextState){
       console.log('[Updated App.js] Inside componentWillUpdate()', nextProps, nextState)
@@ -132,6 +133,7 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
+      <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
         <Cockpit
           appTitle={this.props.title} 
           showPersons={this.state.showPersons} 
