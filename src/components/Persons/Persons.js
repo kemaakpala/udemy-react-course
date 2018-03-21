a6d04cdf69022fd048e47f24b543a8b90fd6ab44
Patrick Akpala <kemaakpala@gmail.com>
2018-03-21 08:39:56 +0000

    component Updating LifeCycle in Action: componentWillReceiveProps(): No SideEffects Allowed shouldComponentUpdate(): No SideEffects Allowed componentWillUpdate: No SideEffects Allowed componentDidUpdate: SideEffects can be caused here e.g accessing a database/ API endpoint

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -7,19 +7,37 @@ class Persons extends Component {
 		super(props)
 		console.log('[Persons.js] inside constructor:', props)
 		
-	}
-	
+    }
+    
+	//Lifecycle methods below are not called through dom events
 	componentWillMount(){
 		console.log('[Persons.js] Inside componentWillMount()')
 	}
 	
 	componentDidMount(){
-		console.log('[Persons.js] Inside componentDidMount')
+		console.log('[Persons.js] Inside componentDidMount(): Side-Effects can be caused here')
 	  
     }
     
     componentWillUnMount(){
         console.log('[Persons.js] Inside componentWillUnMount()')
+    }
+
+    componentWillReceiveProps (nextProps){
+        console.log('[Update Persons.js] Inside componentWillReceiveProps()', nextProps )
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+        console.log('[Updated Persons.js] Inside shouldComponentUpdate()', nextProps, nextState)
+        return nextProps !== this.props.persons
+    }
+
+    componentWillUpdate(nextProps, nextState){
+        console.log('[Updated Persons.js] Inside componentWillUpdate()', nextProps, nextState)
+    }
+
+    componentDidUpdate(){
+        console.log('[Updated Persons.js] Inside componentDidUpdate(): Side-Effects can be caused here', this.props, this.state)
     }
     render() {
         console.log('[Persons.js] Inside render()')
